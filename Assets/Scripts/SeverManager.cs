70023a90a953b7fef70ae7fb829a97b4c40470b6
Peinoi <124995489+Peinoi@users.noreply.github.com>
2023-11-19 21:28:21 +0900

    2

--- Assets/Scripts/SeverManager.cs
+++ Assets/Scripts/SeverManager.cs
@@ -7,64 +7,41 @@ using Photon.Realtime;
 
 public class ServerManager : MonoBehaviourPunCallbacks
 {
-    InputField m_InputField;
-    Text player1_Id;
-    Text player2_Id;
-
     void Start()
     {
-        Screen.SetResolution(960, 600, false);
+        ConnectToPhoton();
+    }
 
-        m_InputField = GameObject.Find("Canvas/InputField").GetComponent<InputField>();
-        m_textPlayerList = GameObject.Find("Canvas/TextPlayerList").GetComponent<Text>();
-        m_textConnectLog = GameObject.Find("Canvas/TextConnectLog").GetComponent<Text>();
+    void ConnectToPhoton()
+    {
+        Debug.Log("Connecting to Photon Server...");
 
-        m_textConnectLog.text = "접속로그\n";
+        // 포톤 서버에 연결
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
-        RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 5;
+        Debug.Log("Connected to Photon Master Server!");
 
-        PhotonNetwork.LocalPlayer.NickName = m_InputField.text;
-        PhotonNetwork.JoinOrCreateRoom("Room1", options, null);
-
-    }
-    public override void OnJoinedRoom()
-    {
-        updatePlayer();
-        m_textConnectLog.text += m_InputField.text;
-        m_textConnectLog.text += " 님이 방에 참가하였습니다.\n";
+        // 연결되면 로비로 입장
+        PhotonNetwork.JoinLobby();
     }
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
+    public override void OnDisconnected(DisconnectCause cause)
     {
-        updatePlayer();
-        m_textConnectLog.text += newPlayer.NickName;
-        m_textConnectLog.text += " 님이 입장하였습니다.\n";
+        Debug.LogWarning($"Disconnected from Photon Server. Reason: {cause}");
+
+        // 연결이 끊기면 다시 연결 시도
+        ConnectToPhoton();
     }
 
-    public override void OnPlayerLeftRoom(Player otherPlayer)
+    public override void OnJoinedLobby()
     {
-        updatePlayer();
-        m_textConnectLog.text += otherPlayer.NickName;
-        m_textConnectLog.text += " 님이 퇴장하였습니다.\n";
-    }
+        Debug.Log("Joined Photon Lobby!");
 
-    public void Connect()
-    {
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    void updatePlayer()
-    {
-        m_textPlayerList.text = "접속자";
-        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
-        {
-            m_textPlayerList.text += "\n";
-            m_textPlayerList.text += PhotonNetwork.PlayerList[i].NickName;
-        }
+        // 로비에 입장했을 때의 추가 작업 수행
+        // 예: 룸 생성 또는 룸에 입장
     }
 
 }
